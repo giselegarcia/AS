da3fc400c3fc66de7874842cae0001844c4fad91
giselegarcia <80002777+giselegarcia@users.noreply.github.com>
2022-07-08 17:57:20 -0300

    codigo

---
+++ assets/main.js
@@ -0,0 +1,8 @@
+/*var info = confirm ("Você lê livros?");
+
+if (info == true){
+    alert("Seja  bem vindo! O clube de Resenhas esta ansioso por sua participação.");
+} else {
+    alert("Divirta-se lendo as resenhas feitas para você.")
+}
+*/
