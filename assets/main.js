0f1d39f978fb861d75f61374ffed5fd9df3deac4
giselegarcia <80002777+giselegarcia@users.noreply.github.com>
2022-07-08 23:01:01 -0300

    at

--- assets/main.js
+++ assets/main.js
@@ -1,8 +1,40 @@
-/*var info = confirm ("Você lê livros?");
 
-if (info == true){
-    alert("Seja  bem vindo! O clube de Resenhas esta ansioso por sua participação.");
-} else {
-    alert("Divirta-se lendo as resenhas feitas para você.")
-}
-*/
+$(document).ready(function(){
+
+    $('section').load('home.html')
+
+    //função load na navbar
+    $('#menu_home').click(function(){
+        $('section').load('home.html', function())
+    })
+    $('#menu_enviar').click(function(){
+        $('section').load('enviar.html',function())
+    })
+    $('#menu_quemsomos').click(function(){
+        $('section').load('quemsomos.html',function())
+    })
+    $('#menu_resenhas').click(function(){
+        $('section').load('resenhas.html',function())
+    })
+
+
+
+
+
+
+    //mostrar e ocultar conteudo
+    $('#mostrar').click(function(){
+        $('#conteudo').show()
+    })
+    $('#ocultar').click(function(){
+        $('#conteudo').hide()
+    })
+
+
+    
+
+
+});
+
+
+
--- assets/validacao.js
+++ assets/validacao.js
@@ -1,29 +1,23 @@
-const form = document.getElementById('form')
-const username = document.getElementById('username')
-const email = document.getElementById('email')
-const texto = document.getElementById('texto')
+const form = document.getElementById("form");
+const username = document.getElementById("username");
+const email = document.getElementById("email");
+
 
 form.addEventListener("submit", (e) => {
-    e.preventDefault();
-  
-    checkInputs();
-  });
+  e.preventDefault();
 
-  function checkInputs() {
-    const usernameValue = username.value;
-    const emailValue = email.value;
-    const texto = texto.value;
-  }
+  checkInputs();
+});
+
+function checkInputs() {
+  const usernameValue = username.value;
+  const emailValue = email.value;
   
+
   if (usernameValue === "") {
     setErrorFor(username, "O nome de usuário é obrigatório.");
   } else {
     setSuccessFor(username);
-  }
-  if (texto=== "") {
-    setErrorFor(texto, "O envio do texto é obrigatório".);
-  } else {
-    setSuccessFor(texto);
   }
 
   if (emailValue === "") {
@@ -33,6 +27,11 @@ form.addEventListener("submit", (e) => {
   } else {
     setSuccessFor(email);
   }
+
+  
+
+ 
+
   const formControls = form.querySelectorAll(".form-control");
 
   const formIsValid = [...formControls].every((formControl) => {
@@ -42,27 +41,28 @@ form.addEventListener("submit", (e) => {
   if (formIsValid) {
     console.log("O formulário está 100% válido!");
   }
+}
 
-  function setErrorFor(input, message) {
-    const formControl = input.parentElement;
-    const small = formControl.querySelector("small");
-  
-    // Adiciona a mensagem de erro
-    small.innerText = message;
-  
-    // Adiciona a classe de erro
-    formControl.className = "form-control error";
-  }
-  
-  function setSuccessFor(input) {
-    const formControl = input.parentElement;
-  
-    // Adicionar a classe de sucesso
-    formControl.className = "form-control success";
-  }
-  
-  function checkEmail(email) {
-    return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
-      email
-    );
-  }
+function setErrorFor(input, message) {
+  const formControl = input.parentElement;
+  const small = formControl.querySelector("small");
+
+  // Adiciona a mensagem de erro
+  small.innerText = message;
+
+  // Adiciona a classe de erro
+  formControl.className = "form-control error";
+}
+
+function setSuccessFor(input) {
+  const formControl = input.parentElement;
+
+  // Adicionar a classe de sucesso
+  formControl.className = "form-control success";
+}
+
+function checkEmail(email) {
+  return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+    email
+  );
+}
