b15bfc1615b7ff43c6ee4eff8fb608c4fcfb0437
giselegarcia <80002777+giselegarcia@users.noreply.github.com>
2022-07-13 20:13:02 -0300

    codigo

--- assets/main.js
+++ assets/main.js
@@ -1,27 +1,6 @@
 
 $(document).ready(function(){
 
-    $('section').load('home.html')
-
-    //função load na navbar
-    $('#menu_home').click(function(){
-        $('section').load('home.html', function())
-    })
-    $('#menu_enviar').click(function(){
-        $('section').load('enviar.html',function())
-    })
-    $('#menu_quemsomos').click(function(){
-        $('section').load('quemsomos.html',function())
-    })
-    $('#menu_resenhas').click(function(){
-        $('section').load('resenhas.html',function())
-    })
-
-
-
-
-
-
     //mostrar e ocultar conteudo
     $('#mostrar').click(function(){
         $('#conteudo').show()
